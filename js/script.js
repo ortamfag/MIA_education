b6da6001ff15cbb80b0e234bbe1633f491c519d3
ortamfag <a_zhidov@outlook.com>
2021-06-16 19:24:10 +0300

    ADD: first empty popup

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@
 const burger = document.querySelector('.menu-burger');
 burger.addEventListener('click', () => burger.classList.toggle('active'));
 // end menu
+
+// modals 
+const makeModal = modalSel => {
+	const modalEl = document.querySelector(modalSel);
+		btnEl = document.querySelector(modalSel + '-btn'),
+		closeEl = document.querySelector(modalSel + ' .modal-wrapper__bg'),
+
+	btnEl.addEventListener('click', () => modalEl.classList.add('active'));
+	closeEl.addEventListener('click', () => modalEl.classList.remove('active'));
+}
+
+makeModal('#modal-1');
+// end modals
